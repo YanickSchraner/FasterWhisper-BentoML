86e9a7a89ea80342c655362935b44d4b4ceda2d7
Adrian Schalbetter <adrian.schalbetter@ti8m.ch>
2024-11-28 10:37:33 +0100

    add ctranslate2 log level to logging config

--- logger.py
+++ logger.py
@@ -1,8 +1,11 @@
 import logging
 
+import ctranslate2
+
 
 def configure_logging():
 
+    ctranslate2.set_log_level(logging.INFO)
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s -%(funcName)s: %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',

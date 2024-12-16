f37f2c6c4a723974ce900a1b3c51ad560e6d6a11
Adrian Schalbetter <adrian.schalbetter@ti8m.ch>
2024-12-16 16:05:32 +0100

    clean up logger

--- logger.py
+++ logger.py
@@ -5,13 +5,8 @@ import ctranslate2
 
 def configure_logging():
 
-    ch = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s.%(msecs)03d %(levelname)s %(module)s -%(funcName)s: %(message)s')
-    ch.setFormatter(formatter)
-
-    bentoml_logger = logging.getLogger("bentoml")
-    bentoml_logger.addHandler(ch)
-    bentoml_logger.setLevel(logging.DEBUG)
-
-    logging.getLogger("faster_whisper").setLevel(logging.DEBUG)
     ctranslate2.set_log_level(logging.INFO)
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s.%(msecs)03d %(levelname)s %(name)s -%(funcName)s: %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
